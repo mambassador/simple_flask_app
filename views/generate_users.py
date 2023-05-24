4872819a977c48e639050d37123f40e046774883
Vladyslav <vladyslavtkach.qa@gmail.com>
2023-05-24 19:31:58 +0300

    Fixed docstrings

--- views/generate_users.py
+++ views/generate_users.py
@@ -10,7 +10,7 @@ def generate_users() -> str:
     """Returns a string with list of mock users.
 
     Returns:
-        mock_users(str): spe
+        mock_users(str): generated mock users
 
     By default, it returns 100 users.
     The query parameter 'count' specifies the total number of users.
